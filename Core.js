cadc11e1f6d3fdffe7f29bf3f1e77c0abdf46d10
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-02-25 14:46:13 +0100

    Latest

---
+++ Core.js
@@ -0,0 +1,24 @@
+let text = `import { PinoResolver } from 'https://pino-sdk.vercel.app/core/pino-protocol.js';
+import Pine from 'https://pino-sdk.vercel.app/core/pine.js';
+
+let app = new Pine();
+app.resolve(new PinoResolver(), startPine);
+app.on('offline', showError);
+// app.on('init', startPine);
+
+function startPine() {
+	let code = document.querySelector('.code code')
+	app.fetch('get', \`pine.notes.\${app.params.note}\`, (data)=>{
+		code.innerHTML = data.content;
+		// code.className = \`h-full language-\${data.lang}\`;
+		
+		document.querySelector('#date').innerHTML = data.created.substring(0,10).replace(/\-/g, "/");
+		hljs.highlightAll();
+	});
+} 
+
+function showError() {
+	alert('Offline')
+}
+`
+console.log(JSON.stringify({content: text}))
