52e4ec7696acb2be360078c44c71874ec8b6750f
ifedayoprince@gmail.com <ifedayoprince@gmail.com>
2023-03-18 20:15:03 +0100

    Updated compression

--- Tet.js
+++
@@ -1,6 +0,0 @@
-function hello() {
-	console.log('Hello Pinochs 👋!! ');
-}
-
-hello();
-// Hello Pinochs 👋!! 
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import Pine from 'https://pino-sdk.vercel.app/core/pine.js';
 let app = new Pine();
 app.resolve(new PinoResolver(), startPine);
 app.on('offline', showError);
-// app.on('init', startPine);
+app.on('init', startPine);
 
 function startPine() {
 	let code = document.querySelector('.code code')
+	var script = document.createElement("script");
 
 	app.fetch(`pine.notes.${app.params.note}`).then((data)=>{
 		data = data.data;
@@ -16,9 +17,19 @@ function startPine() {
 		code.innerHTML = data.content;
 		code.parentElement.classList.remove('hidden');
 		
-		// code.className = `h-full language-${data.lang}`;
+		code.className = `h-full language-${app.params.lang}`;
 		document.querySelector('#date').innerHTML = data.created.substring(0,10).replace(/\-/g, "/");
-		hljs.highlightAll();
+
+		let langs = {
+			js: "javascript",
+			py: "python"
+		}
+
+		script.src = `https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/languages/${langs[app.params.lang]}.min.js`
+		script.onload = ()=>{
+			hljs.highlightAll();
+		}
+		document.body.appendChild(script);
 	}).catch((e)=>{
 		console.log(e.message)
 	});
